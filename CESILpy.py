88599b410c3fc0b03ae436568bec56ac1e8c4aeb
Alan J. Salmoni <salmoni@gmail.com>
2016-12-24 23:26:57 +0000

    Updated format and improved parsing
    CESILPy now accepts something closer to how programs are meant to be.
    
    Column 1 (characters 0-7) is for labels. Column 2 is for operators
    (commands: characters 8-15) and anything else is column 3 which is for
    identifiers (variable names or labels), strings or integers.

--- CESILpy.py
+++ CESILpy.py
@@ -24,6 +24,7 @@ class CESILObj(object):
             print("Cannot close the file %s but will continue nevertheless"%fname)
         self.fname = fname
         self.script = script.split(os.linesep)
+        self.ParseScript()
         self.commands = ["LOAD","STORE","JUMP","JINEG","JIZERO","PRINT","OUT",
                 "IN","LINE","HALT","ADD","SUBTRACT","MULTIPLY","DIVIDE"]
         self.acc = 0 # accumulator
@@ -33,22 +34,48 @@ class CESILObj(object):
         self.ParseVariables()
         self.RunScript()
 
+    def ParseScript(self):
+        self.newscript = []
+        for line in self.script:
+            line_len = len(line)
+            if len(line) > 0:
+                # is not an empty line
+                if line[0] == '%':
+                    pass
+                else:
+                    if line_len < 9:
+                        # we have a label space only
+                        word1 = line.strip(' ')
+                        word2 = ''
+                        word3 = ''
+                    elif line_len < 17:
+                        # we have label space and command only
+                        word1 = line[0:8].strip(' ')
+                        word2 = line[8:16].strip(' ')
+                        word3 = ''
+                    else:
+                        word1 = line[0:8].strip(' ')
+                        word2 = line[8:16].strip(' ')
+                        word3 = line[16:]
+                        if '"' not in word3:
+                            # is not a string
+                            word3 = word3.strip(' ')
+                    self.newscript.append([word1, word2, word3])
+
     def ParseLabels(self):
         """
         This runs through and extracts all label names and stores them with the
         line number.
         Duplicates are not allowed and cause termination.
         """
-        for idx, line in enumerate(self.script):
-            words = line.split()
-            if len(words) > 0:
-                initword = words[0]
-                if initword not in self.commands:
-                    if initword not in self.labels.keys():
-                        self.labels[initword] = idx
-                    else:
-                        print("### ERROR ### DUPLICATE LABEL ON LINE "+str(idx))
-                        sys.exit()
+        for idx, line in enumerate(self.newscript):
+            if len(line[0]) > 0:
+                initword = line[0]
+                if initword not in self.labels.keys():
+                    self.labels[initword] = idx
+                else:
+                    print("### ERROR ### DUPLICATE LABEL ON LINE "+str(idx))
+                    sys.exit()
 
     def ParseVariables(self):
         """
@@ -56,17 +83,15 @@ class CESILObj(object):
         All variables are initialised to zero (0)
         """
         vars = {}
-        for idx, line in enumerate(self.script):
-            words = line.split()
+        for idx, line in enumerate(self.newscript):
             try:
-                poss_var = words[-1]
-                if poss_var not in self.commands:
-                    if poss_var[0] != '"':
-                        if poss_var not in self.labels:
-                            try:
-                                val = int(poss_var)
-                            except ValueError: # should not be integer
-                                self.variables[poss_var] = 0
+                poss_var = line[2]
+                if poss_var[0] != '"':
+                    if poss_var not in self.labels:
+                        try:
+                            val = int(poss_var)
+                        except ValueError: # should not be integer
+                            self.variables[poss_var] = 0
             except IndexError:
                 pass
 
@@ -78,7 +103,7 @@ class CESILObj(object):
         print("%")
         self.linenum = 0
         while True:
-            line = self.script[self.linenum]
+            line = self.newscript[self.linenum]
             #print ("LN = ",str(self.linenum+1))
             old_line = self.linenum
             res = self.parse_line(line)
@@ -88,71 +113,59 @@ class CESILObj(object):
                     self.halt_good()
 
     def parse_line(self, line):
-        # Split the line into words
-        # First by partitioning quotes out (strings)...
-        strings = line.split('"')
-        # ...then but "chunk"
-        words = strings[0].split()
-        try:
-            words.append(strings[1])
-        except IndexError:
-            pass
-        # Calculate the number of words
-        num_words = len(words)
         # Run through commands in turn
-        if num_words > 0:
-            if "HALT" in words:
-                self.halt_good()
-            elif "IN" in words:
-                print ("? ",end="")
-                while True:
-                    val = input()
-                    try:
-                        self.acc = int(val)
-                        break
-                    except ValueError:
-                        print("REDO? ",end="")
-            elif "OUT" in words:
-                print (str(self.acc), end="")
-            elif "LINE" in words:
-                print ("\n")
-            elif "LOAD" in words[:-1]:
-                operand = words[-1]
+        label = line[0]
+        operator = line[1]
+        operand = line[2]
+        if operator == "HALT":
+            self.halt_good()
+        elif operator == "IN":
+            print ("? ",end="")
+            while True:
+                val = input()
                 try:
-                    self.acc = int(operand)
+                    self.acc = int(val)
+                    break
                 except ValueError:
-                    self.acc = self.variables[operand]
-            elif "STORE" in words[:-1]:
-                var_name = words[-1]
-                self.variables[var_name] = self.acc
-            elif "PRINT" in words[:-1]:
-                print (words[-1], end="")
-            elif ("ADD" in words[:-1]) or ("SUBTRACT" in words[:-1]) or ("MULTIPLY" in words[:-1]) or ("DIVIDE" in words[:-1]):
-                operand = words[-1]
-                try:
-                    value = int(operand)
-                except ValueError:
-                    value = self.variables[operand]
-                if "ADD" in words[:-1]:
-                    self.acc = self.acc + value
-                elif "SUBTRACT" in words[:-1]:
-                    self.acc = self.acc - value
-                elif "MULTIPLY" in words[:-1]:
-                    self.acc = self.acc * value
-                elif "DIVIDE" in words[:-1]:
-                    if value == 0:
-                        print ("### ERROR ### DIVIDE BY ZERO LINE "+str(self.linenum))
-                        sys.exit()
-                    else:
-                        self.acc = int(self.acc / value)
-            elif "JUMP" in words[:-1]:
+                    print("REDO? ",end="")
+        elif operator == "OUT":
+            print (str(self.acc), end="")
+        elif operator == "LINE":
+            print ("\n",end="")
+        elif operator == "LOAD":
+            try:
+                self.acc = int(operand)
+            except ValueError:
+                self.acc = self.variables[operand]
+        elif operator == "STORE":
+            self.variables[operand] = self.acc
+        elif operator == "PRINT":
+            print (operand.strip('"').strip('\n'), end="")
+        elif (operator == "ADD") or (operator == "SUBTRACT") or (operator == "MULTIPLY") or (operator == "DIVIDE"):
+            try:
+                value = int(operand)
+            except ValueError:
+                value = self.variables[operand]
+            if operator == "ADD":
+                self.acc = self.acc + value
+            elif operator == "SUBTRACT":
+                self.acc = self.acc - value
+            elif operator == "MULTIPLY":
+                self.acc = self.acc * value
+            elif operator == "DIVIDE":
+                if value == 0:
+                    print ("### ERROR ### DIVIDE BY ZERO LINE "+str(self.linenum))
+                    sys.exit()
+                else:
+                    self.acc = int(self.acc / value)
+        elif operator == "JUMP":
+            self.linenum = self.labels[words[-1]]
+        elif operator == "JIZERO":
+            if self.acc == 0:
                 self.linenum = self.labels[words[-1]]
-            elif "JIZERO" in words[:-1]:
-                if self.acc == 0:
-                    self.linenum = self.labels[words[-1]]
-            elif "JINEG" in words[:-1]:
-                if self.acc < 0:
-                    self.linenum = self.labels[words[-1]]
+        elif operator == "JINEG":
+            if self.acc < 0:
+                self.linenum = self.labels[words[-1]]
 
 if __name__ == "__main__":
     try:
